7f3423d5ab642a4f8e8e7d1e77e2355ce6f8dcde
Phoenix <phoenixwb@netc.eu>
2023-05-02 11:21:36 -0400

    create ui, options, logger frameworks

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Working Title Game(ICS3U Final Project)
 
 Simple light fantasy MUD(Multi-User Dungeon) game.
+Documentation by real humans trust me bro
---
+++ src/main/java/coffeemud/ui.java
@@ -0,0 +1,13 @@
+package coffeemud;
+
+public class ui {
+    public static class stage {
+        public void createMenu() {
+            logger.debug("Creating menu");
+        }
+
+    }
+    public stage createStage() {
+        return new stage();
+    }
+}
