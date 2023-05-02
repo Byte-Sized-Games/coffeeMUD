839c5597da8bdaa72a344dd0a9a91cc2a8ec70a0
Phoenix <phoenixwb@netc.eu>
2023-05-02 11:30:24 -0400

    create intellij run config & add argument handling

--- src/main/java/coffeemud/Main.java
+++ src/main/java/coffeemud/Main.java
@@ -2,6 +2,16 @@ package coffeemud;
 
 public class Main {
     public static void main(String[] args) {
+        for(String arg : args) {
+            switch(arg) {
+                case "--debug":
+                    logger.debug("Debug mode enabled");
+                    break;
+                default:
+                    logger.info("Unknown argument: " + arg);
+                    break;
+            }
+        }
         System.out.println("Hello, World!");
     }
 }
--- src/main/java/coffeemud/ui.java
+++ src/main/java/coffeemud/ui.java
@@ -5,7 +5,6 @@ public class ui {
         public void createMenu() {
             logger.debug("Creating menu");
         }
-
     }
     public stage createStage() {
         return new stage();
