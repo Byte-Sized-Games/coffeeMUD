be015016a68e651fd6d5ec83179989eab67b554d
Michael Ward <gecotron@gmail.com>
2023-05-03 10:02:37 -0400

    added ansi colours for easy access

---
+++ src/main/java/coffeemud/colours.java
@@ -0,0 +1,13 @@
+package coffeemud;
+
+public class colours {
+    String red = "\u001b[31m";
+    String yellow = "\u001b[33m";
+    String green = "\u001b[32m";
+    String blue = "\u001b[34m";
+    String magenta = "\u001b[35m";
+    String cyan = "\u001b[36m";
+    String black = "\u001b[30m";
+    String white = "\u001b[37m";
+    String reset = "\u001b[0m";
+}
