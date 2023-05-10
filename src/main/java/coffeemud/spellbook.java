88c78394cc4844ee6e82ea30385b2c9cc2c68ea1
Michael Ward <gecotron@gmail.com>
2023-05-10 06:37:36 -0400

    spell scrolls finished

--- src/main/java/coffeemud/spellbook.java
+++ src/main/java/coffeemud/spellbook.java
@@ -13,12 +13,21 @@ public class spellbook {
             new spell('d', 10, 'n', 2, "Psionic Blast", "A powerful psionic blast of magic energy"),
             new spell('x', 3, 'b', 2, "Sleep", "Send enemies to sleep, leaving them vulnerable"),
             new spell('d', 15, 'n', 3, "Fireball", "An intermediate fire spell. Great against creatures of ice"),
-            new spell('d', 6, 'e', 3, "Lightning Bolt", "A intermediate electricity spell. Great against creatures of fire"),
-            new spell('d', 6, 'i', 3, "Ice Blast", "A intermediate ice spell. Great against creatures of earth"),
-            new spell('d', 6, 'P', 3, "Roots", "A intermediate earth spell. Great against creatures of electricity"),
+            new spell('d', 15, 'e', 3, "Lightning Bolt", "A intermediate electricity spell. Great against creatures of fire"),
+            new spell('d', 15, 'i', 3, "Ice Blast", "A intermediate ice spell. Great against creatures of earth"),
+            new spell('d', 15, 'P', 3, "Roots", "A intermediate earth spell. Great against creatures of electricity"),
             new spell('b', 5, 'B', 3, "Barrier", "A powerful spell of protection"),
             new spell('b', 20, 'B', 4, "Mage Armor", ""),
             new spell('b', 0, 's', 4, "Summon elemental", "Summon a powerful elemental to fight by your side")
+        };
+    }
+    public class cleric {}
+    public class scroll {
+        spell[] spells = new spell[] {
+            new spell('d', 35, 'f', 4, "Fire Storm", "A powerful fire spell. Destructive againt creatures of ice"),
+            new spell('d', 35, 'e', 4, "Thunder Storm", "A powerful electricity spell. Destructive againt creatures of fire"),
+            new spell('d', 35, 'i', 4, "Ice Storm", "A powerful ice spell. Destructive againt creatures of earth"),
+            new spell('d', 35, 'P', 4, "Vines", "A powerful earth spell. Destructive againt creatures of electricity"),
         };
     }
 }
