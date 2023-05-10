b00c30fe96cf5e0881377006d340933869a23a49
Michael Ward <gecotron@gmail.com>
2023-05-10 05:22:15 -0400

    colour suggestions (check comments in code)

--- src/main/java/coffeemud/colours.java
+++ src/main/java/coffeemud/colours.java
@@ -1,12 +1,12 @@
 package coffeemud;
 
 public class colours {
-    String red = "\u001b[31m";
-    String yellow = "\u001b[33m";
-    String green = "\u001b[32m";
-    String blue = "\u001b[34m";
-    String magenta = "\u001b[35m";
-    String cyan = "\u001b[36m";
+    String red = "\u001b[31m"; // Enemy
+    String yellow = "\u001b[33m"; // Event
+    String green = "\u001b[32m"; // Player
+    String blue = "\u001b[34m"; // Debug Message
+    String magenta = "\u001b[35m"; // System Message
+    String cyan = "\u001b[36m"; // Important object/entity
     String black = "\u001b[30m";
     String white = "\u001b[37m";
     String reset = "\u001b[0m";
