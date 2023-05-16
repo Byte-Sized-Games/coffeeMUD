897a6d363a9d897dab80274a0cffd1eabf7143b0
Michael Ward <gecotron@gmail.com>
2023-05-16 14:26:55 -0400

    add alt UI incase other version cannot be finished

---
+++ src/main/java/coffeemud/uiAlt.java
@@ -0,0 +1,20 @@
+package coffeemud;
+
+public class uiAlt {
+    // Menu for managing inventory
+    public class invMenu {
+
+    }
+    // Second most used menu. Used when fighting monsters
+    public class battle {
+
+    }
+    // The most used screen/menu. Used when in dungeons and towns
+    public class explorationMenu {
+
+    }
+    // Automate Dialogue from characters and world
+    public class dialogue {
+
+    }
+}
