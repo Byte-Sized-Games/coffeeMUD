64ddc19a80ca922b184c337dd3aa8458b1dd18d1
Michael Ward <gecotron@gmail.com>
2023-05-25 11:04:59 -0400

    basic combat

--- src/main/java/coffeemud/uiAlt.java
+++ src/main/java/coffeemud/uiAlt.java
@@ -12,9 +12,11 @@ public class uiAlt {
             System.out.print("\033[H\033[2J");
             System.out.flush();
         }
+
         public void show(battle m) {
 
         }
+
         public void show(String m) {
             gamePrompt.print(m);
         }
@@ -38,6 +40,7 @@ public class uiAlt {
             }
             return numBld.toString();
         }
+
         // Unumbered list, for inventories and graphics
         public String unNumList(String[] x) {
             StringBuilder numBld = new StringBuilder();
@@ -58,8 +61,87 @@ public class uiAlt {
 
     // Used when fighting monsters
     public class battle {
+        ArrayList<entities> players;
         ArrayList<entities> monsters;
+        int round;
+        int playerInitiative;
+        int monsterInitiative;
+        prompt battlePrompt;
+
+        public battle(ArrayList<entities> p, ArrayList<entities> m, int pInitiative, int mInitiative) {
+            this.init(p, m, pInitiative, mInitiative);
+        }
+
+        public void init(ArrayList<entities> p, ArrayList<entities> m, int pI, int mI) {
+            this.players = p;
+            this.monsters = m;
+            this.round = 0;
+            this.playerInitiative = pI;
+            this.monsterInitiative = mI;
+        }
+
+        public void turn() {
+            if (playerInitiative >= monsterInitiative) {
+                for (entities i : players) {
+                    playerTurn(i);
+                }
+                for (entities i : monsters) {
+                    monsterTurn(i);
+                }
+            } else {
+                for (entities i : monsters) {
+                    monsterTurn(i);
+                }
+                for (entities i : players) {
+                    playerTurn(i);
+                }
+            }
+        }
+        public void playerTurn(entities player) {
+            String input = battlePrompt.read();
+            input = input.toUpperCase();
+            String command = input.substring(0, input.indexOf(" ") + 1);
+            String target = input.substring(input.indexOf(" "));
+            switch (command) {
+                case "ATTACK":
+                    player.attack(monsters.get(getMonster(target)));
+                case "CAST":
+                    castSpell();
+                case "DEFEND":
+                    players.get(getPlayer(target)).armour += 2;
+                case "":
+                    battlePrompt.print("No command chosen, please try again");
+                default:
+                    battlePrompt.print("Command not recognized");
+            }
+        }
+        public void monsterTurn(entities monster) {
+            if (monster.health <= (monster.maxHealth / 4)) {
+                monster.health += monster.heal;
+            } else {
+                monster.attack(players.get((int) (Math.random() * (players.size() - 1)) + 1));
+            }
+        }
+        public int getMonster(String target) {
+            int i = 0;
+            for (entities x : monsters) {
+                if (x.name.equals(target)) {
+                    break;
+                } else i++;
+            }
+            return i;
+        }
+        public int getPlayer(String target) {
+            int i = 0;
+            for (entities x : players) {
+                if (x.name.equals(target)) {
+                    break;
+                } else i++;
+            }
+            return i;
+        }
     }
+
     // A prompt for players to input text into
     public class prompt {
         String back;
