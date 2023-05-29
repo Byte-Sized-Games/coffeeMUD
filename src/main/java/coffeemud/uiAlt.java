c67e85878e382893ef0d372dcc0614869766860d
Michael Ward <gecotron@gmail.com>
2023-05-29 09:48:58 -0400

    remove battle class (temporary)

--- src/main/java/coffeemud/uiAlt.java
+++ src/main/java/coffeemud/uiAlt.java
@@ -1,6 +1,5 @@
 package coffeemud;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class uiAlt {
@@ -59,88 +58,7 @@ public class uiAlt {
         }
     }
 
-    // Used when fighting monsters
-    public class battle {
-        ArrayList<entities> players;
-        ArrayList<entities> monsters;
-        int round;
-        int playerInitiative;
-        int monsterInitiative;
-        prompt battlePrompt;
 
-        public battle(ArrayList<entities> p, ArrayList<entities> m, int pInitiative, int mInitiative) {
-            this.init(p, m, pInitiative, mInitiative);
-        }
-
-        public void init(ArrayList<entities> p, ArrayList<entities> m, int pI, int mI) {
-            this.players = p;
-            this.monsters = m;
-            this.round = 0;
-            this.playerInitiative = pI;
-            this.monsterInitiative = mI;
-        }
-
-        public void turn() {
-            if (playerInitiative >= monsterInitiative) {
-                for (entities i : players) {
-                    playerTurn(i);
-                }
-                for (entities i : monsters) {
-                    monsterTurn(i);
-                }
-            } else {
-                for (entities i : monsters) {
-                    monsterTurn(i);
-                }
-                for (entities i : players) {
-                    playerTurn(i);
-                }
-            }
-        }
-        public void playerTurn(entities player) {
-            String input = battlePrompt.read();
-            input = input.toUpperCase();
-            String command = input.substring(0, input.indexOf(" ") + 1);
-            String target = input.substring(input.indexOf(" "));
-            switch (command) {
-                case "ATTACK":
-                    player.attack(monsters.get(getMonster(target)));
-                case "CAST":
-                    castSpell();
-                case "DEFEND":
-                    players.get(getPlayer(target)).armour += 2;
-                case "":
-                    battlePrompt.print("No command chosen, please try again");
-                default:
-                    battlePrompt.print("Command not recognized");
-            }
-        }
-        public void monsterTurn(entities monster) {
-            if (monster.health <= (monster.maxHealth / 4)) {
-                monster.health += monster.heal;
-            } else {
-                monster.attack(players.get((int) (Math.random() * (players.size() - 1)) + 1));
-            }
-        }
-        public int getMonster(String target) {
-            int i = 0;
-            for (entities x : monsters) {
-                if (x.name.equals(target)) {
-                    break;
-                } else i++;
-            }
-            return i;
-        }
-        public int getPlayer(String target) {
-            int i = 0;
-            for (entities x : players) {
-                if (x.name.equals(target)) {
-                    break;
-                } else i++;
-            }
-            return i;
-        }
-    }
 
     // A prompt for players to input text into
     public class prompt {
