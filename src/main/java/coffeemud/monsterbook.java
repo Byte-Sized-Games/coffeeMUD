6f9bc139b9e0e23daba9fbb7ac0c162735c14177
Phoenix <phoenixwb@netc.eu>
2023-06-17 17:23:18 -0400

    that's what the point of the mask is

--- src/main/java/coffeemud/monsterbook.java
+++ src/main/java/coffeemud/monsterbook.java
@@ -366,15 +366,4 @@ public class monsterbook {
     public static entities createBat()  {
         return new entities("Bat",30,1,10,0);
     }
-    /*
-    tatic entities goblin = new entities("Goblin", 25, 1, 20, 2);
-
-        static entities bat = new entities("Bat", 30, 1, 10, 0);
-
-        static entities witch = new entities("Witch", 75, 20, 50, 0);
-
-        static entities skeleton = new entities("Skeleton", 50, 5, 35, 5);
-
-        static entities troll = new entities("Troll", 100, 10, 40, 10);
-     */
 }
