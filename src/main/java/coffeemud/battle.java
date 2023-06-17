b507833242524636f0efa2fda3c94af95e3cea1f
Michael Ward <gecotron@gmail.com>
2023-06-17 18:19:41 -0400

    tired

--- src/main/java/coffeemud/battle.java
+++ src/main/java/coffeemud/battle.java
@@ -21,10 +21,7 @@ public class battle {
     }
 
     public boolean fight() throws Exception {
-        while (monsters.size() > 0) {
             turn();
-
-        }
         return true;
     }
 
